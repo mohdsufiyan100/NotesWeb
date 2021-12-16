78e31936b2c5b228b9fce613ebe9a43b8c0bd242
sufiyan <ms9114086@gmail.com>
2021-12-16 22:30:29 +0530

    adding some more functionality

--- notes.js
+++ notes.js
@@ -1,6 +1,7 @@
+
+
+
 const btnMain = document.getElementById('btn');
-
-
 
 createElement();
 btnMain.addEventListener('click', () => {
@@ -34,26 +35,29 @@ btnMain.addEventListener('click', () => {
 
 
 function createElement() {
-    let notesarea = document.querySelector('.notesarea');
+    let notesarea = document.getElementById('notesarea');
     let content = localStorage.getItem('Note');
     // let notesINP = document.getElementById('inp2').value;
 
     let html = "";
     if (content == null) {
-        TitalObj = [];
+       TitalObj = [];
+       let emptydata = document.getElementById('emptyarea');
+        emptydata.style.display = 'flex'; 
     } else {
         TitalObj = JSON.parse(content);
+        let emptydata = document.getElementById('emptyarea');
+        emptydata.style.display = 'none';
     } 
     TitalObj.forEach(function(element, index){
         html += `<div class="notes" id="${index}" onclick="fullData(this.id)">
                     <div class="icon">
-                        <i class="far fa-times-circle" id="del${index}" onclick="deleteNote(this.id)"></i>
+                        <i class="far fa-times-circle" id="${index}" onclick="deleteNote(this.id)"></i>
                     </div>
                     <h2>${element.tital}</h2>
                     <p>${element.notes}</p>
                 </div>`;
     });
-    
     notesarea.innerHTML = html;
     
 }
@@ -77,3 +81,60 @@ function deleteNote(index) {
     localStorage.setItem('Note', JSON.stringify(TitalObj));
     createElement();
 }
+
+
+// search function-------
+
+const searchINP = document.getElementById('inpSearch');
+
+searchINP.addEventListener('input', () => {
+    let searchValue = searchINP.value;
+    let card = document.getElementsByClassName('notes');
+    Array.from(card).forEach(function(element) {
+        let cardelem = element.getElementsByTagName('h2')[0].innerText;
+        let searchinnervalue = cardelem.includes(searchValue);
+        if(searchinnervalue){
+            element.style.display = 'inline-flex';
+        }else{
+            element.style.display = 'none';
+        }
+        
+    }) 
+})
+
+
+let Alldelete = document.getElementById('deleteAll');
+
+Alldelete.addEventListener('click', () => {
+    DeleteAll();
+    createElement();
+})
+
+function DeleteAll(){
+    let content = localStorage.getItem('Note');
+
+    let confirm = window.confirm('Would you delete all notes?');
+    if(confirm == false){
+        location.reload();
+    }else{
+        localStorage.clear(content);
+    }
+}
+    
+// search-----------------
+
+let expandsearch = document.getElementById('plusExpand');
+
+expandsearch.addEventListener('click', () => {
+    let searchArea = document.getElementById('search');
+    if(searchArea.style.display == 'block'){
+        searchArea.style.display = 'none';
+        let expandsearch = document.getElementById('plusExpand');
+        expandsearch.style.transform = 'none';
+    }
+    else{
+        searchArea.style.display = 'block';
+        let expandsearch = document.getElementById('plusExpand');
+        expandsearch.style.transform = 'rotate(25deg)';
+    }
+})
