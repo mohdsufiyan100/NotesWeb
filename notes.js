9f197211abd615f64a383663819f4ae20569cc8a
sufiyan <ms9114086@gmail.com>
2021-12-20 22:35:41 +0530

    add editNote functionality

--- notes.js
+++ notes.js
@@ -50,20 +50,66 @@ function createElement() {
         emptydata.style.display = 'none';
     } 
     TitalObj.forEach(function(element, index){
-        html += `<div class="notes" id="${index}" onclick="fullData(this.id)">
+        html += `<div class='fullElement'>
                     <div class="icon">
+                        <i class="far fa-file-edit" id="${index}" onclick="NotesEdit(this.id)"></i>
                         <i class="far fa-times-circle" id="${index}" onclick="deleteNote(this.id)"></i>
                     </div>
-                    <h2>${element.tital}</h2>
-                    <p>${element.notes}</p>
-                </div>`;
+    
+                    <div class="notes" id="full${index}" onclick="fullData(this.id)">
+                        <h2 id="ttl${index}">${element.tital}</h2>
+                        <p id="ntt${index}">${element.notes}</p>
+                    </div>
+                </div>
+                `;
     });
     notesarea.innerHTML = html;
     
 }
 
-function fullData(index) {
-    let notesdiv = document.getElementById(index);
+function NotesEdit(edt1) {
+    const forEdit = document.getElementById('editnote');
+    let forcloseEdit = document.getElementById('editnoteClose');
+    const titalforedit = document.getElementById(`ttl${edt1}`);
+    const noteforedit = document.getElementById(`ntt${edt1}`);
+    const titalfromeditform = document.getElementById('tital');
+    const notesfromeditform = document.getElementById('txtarea');
+    const html1 = titalforedit.innerText;
+    const html2 = noteforedit.innerText;
+    
+    titalfromeditform.innerText = html1;
+    notesfromeditform.innerText = html2;
+    
+    forEdit.style.display = 'block';
+    forcloseEdit.addEventListener('click', () => {
+        forEdit.style.display = 'none';
+    })
+    
+
+    const dataBlur = document.getElementById('DT');
+    dataBlur.addEventListener('click', () => {
+        const blurdt1 = document.getElementById('tital');
+        const blurdt2 = document.getElementById('txtarea');
+    
+        titalforedit.innerText = blurdt1.value;
+        noteforedit.innerText= blurdt2.value;
+        let myobj = {
+            tital: blurdt1.value,
+            notes: blurdt2.value,
+        }
+        TitalObj.push(myobj);
+        localStorage.setItem('Note', JSON.stringify(TitalObj));
+        forEdit.style.display = 'none';
+        createElement();
+    })
+    
+    
+}
+
+function fullData(ind) {
+    // let fully = `full${index}`;
+    
+    let notesdiv = document.getElementById(ind);
     notesdiv.classList.toggle('showfullData');
     notesdiv.style.cursor = 'zoom-out';
 }
@@ -89,7 +135,7 @@ const searchINP = document.getElementById('inpSearch');
 
 searchINP.addEventListener('input', () => {
     let searchValue = searchINP.value;
-    let card = document.getElementsByClassName('notes');
+    let card = document.getElementsByClassName('fullElement');
     Array.from(card).forEach(function(element) {
         let cardelem = element.getElementsByTagName('h2')[0].innerText;
         let searchinnervalue = cardelem.includes(searchValue);
